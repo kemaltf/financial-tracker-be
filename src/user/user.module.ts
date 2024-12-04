9f74819eb0bb444f27d2898e5c6ffb71f269d48a
kemal <kemaltaufikf@gmail.com>
2024-12-04 17:58:00 +0700

    refactor(user): refactor user module

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,13 +1,11 @@
 // src/user/user.module.ts
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
-import { UserController } from './user.controller';
 import { User } from './user.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
-  controllers: [UserController],
   providers: [UserService],
   exports: [UserService],
 })
