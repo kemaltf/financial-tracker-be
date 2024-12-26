b0c8fd8086570d0d8e29d36dbcfbf40b37a8efbd
kemal <kemaltaufikf@gmail.com>
2024-12-26 12:50:50 +0700

    m

---
+++ src/database/database.config.ts
@@ -0,0 +1,19 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { ConfigService } from '@nestjs/config';
+
+export const getDatabaseConfig = (
+  configService: ConfigService,
+): TypeOrmModuleOptions =>
+  ({
+    autoLoadEntities: true,
+    database: configService.get<string>('DB_NAME'),
+    entities: ['dist/**/*.entity.js'], // Sesuaikan dengan build project Anda
+    host: configService.get<string>('DB_HOST'),
+    logging: true,
+    migrations: ['dist/migrations/*.js'],
+    password: configService.get<string>('DB_PASSWORD'),
+    port: configService.get<number>('DB_PORT'),
+    synchronize: configService.get<boolean>('DB_SYNC', false), // Default ke false
+    type: 'mysql', // Pastikan ini adalah literal, bukan string biasa
+    username: configService.get<string>('DB_USERNAME'),
+  }) as const;
--- src/database/seed-runner.ts
+++ src/database/seed-runner.ts
@@ -1,42 +1,5 @@
-import { DataSource } from 'typeorm';
 import { SeederManager } from './seeders';
-import { TransactionType } from 'src/transactionType/transaction-type.entity';
-import { Account } from 'src/account/account.entity';
-import { Transaction } from 'src/transaction/transaction.entity';
-import { AccountingEntry } from 'src/accountingEntry/accounting_entry.entity';
-import { User } from 'src/user/user.entity';
-import { Wallet } from 'src/wallet/wallet.entity';
-import { TransactionDetail } from 'src/transactionDetail/transaction-detail.entity';
-import { Category } from 'src/category/category.entity';
-import { Store } from 'src/store/store.entity';
-import { TransactionLog } from 'src/transactionLogs/transaction-log.entity';
-import { TransactionAddress } from 'src/transactionAddress/transaction-address.entity';
-import { Product } from 'src/product/entity/product.entity';
-
-const AppDataSource = new DataSource({
-  type: 'mysql', // or your DB type
-  host: 'localhost', // your DB host
-  port: 3306, // your DB port
-  username: 'user', // your DB username
-  password: 'password', // your DB password
-  database: 'test', // your DB name
-  entities: [
-    TransactionType,
-    Transaction,
-    Account,
-    AccountingEntry,
-    User,
-    Wallet,
-    Transaction,
-    TransactionDetail,
-    Product,
-    Category,
-    Store,
-    TransactionLog,
-    TransactionAddress,
-  ],
-  synchronize: true, // Jangan aktifkan di production
-});
+import AppDataSource from './orm.config';
 
 AppDataSource.initialize()
   .then(async () => {
